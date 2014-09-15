c9cad98bc4013d1be11f126a167ac8ed20bcecee
v1nt <ma1n.v1nt@gmail.com>
2014-09-16 00:29:51 +0300

    Fraction is upgraded: 2 <= radix <= 36

--- Fraction/Fraction.java
+++ Fraction/Fraction.java
@@ -7,13 +7,15 @@ import java.util.Scanner;
 public class Fraction {
 	private static final String INPUT_FILE_NAME = "in.txt";
 	private static final String DATA_DELIMITER = "\\s+";
-	private static final String FRACTION_PATTERN = "(\\d+)" + DATA_DELIMITER 
-			+ "(\\d+)" + DATA_DELIMITER + "(\\d+)";
-	private static final int INDEX_DIVIDEND = 0;
-	private static final int INDEX_DIVIDER = 1;
-	private static final int INDEX_RADIX = 2;
+	private static final String FRACTION_PATTERN = "(-?\\d+)" + DATA_DELIMITER 
+			+ "(-?\\d+)" + DATA_DELIMITER + "(\\d+)";
+	private static final int DIVIDEND = 0;
+	private static final int DIVIDER = 1;
+	private static final int RADIX = 2;
 	private static final char[] DIGITS = {'0', '1', '2', '3', '4', '5', '6', 
-			'7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+			'7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
+			'x', 'y', 'z'};
 
 	public static void main(String[] args) {
 		try {
@@ -22,23 +24,39 @@ public class Fraction {
 				String scannedLine = scanner.nextLine().trim();
 				if(scannedLine.matches(FRACTION_PATTERN)) {
 					String[] fraction = scannedLine.split(DATA_DELIMITER);
-					int dividend = Integer.parseInt(fraction[INDEX_DIVIDEND]);
-					int divider = Integer.parseInt(fraction[INDEX_DIVIDER]);
-					int radix = Integer.parseInt(fraction[INDEX_RADIX]);
-					if(radix < 1 || radix > DIGITS.length) {
+					long dividend, divider, radix;
+					try {
+						dividend = Long.parseLong(fraction[DIVIDEND]);
+						divider = Long.parseLong(fraction[DIVIDER]);
+						radix = Long.parseLong(fraction[RADIX]);
+					} catch(NumberFormatException e) {
+						System.out.println("bad data format line");
 						continue;
 					}
-					int integerPart = dividend / divider;
-					int nominator = dividend % divider;
-					int denominator = divider;
+					if(radix < 2 || radix > DIGITS.length) {
+						System.out.print("radix must be 1 < radix < ");
+						System.out.println(DIGITS.length);
+						continue;
+					}
 
-					System.out.print(integerPart + " + " + nominator + "/" 
-							+ denominator);
-							
-					StringBuilder digits = convertFraction(nominator, 
-							denominator, radix);
-					System.out.println(" in radix = " + radix + " : " 
-							+ integerPart + "." + digits);
+					long integerPart = Math.abs(dividend / divider);
+					long nominator = Math.abs(dividend) % Math.abs(divider);
+					long denominator = Math.abs(divider);
+
+					StringBuilder fractionPart = translateFraction(radix, 
+							nominator, denominator);
+
+					StringBuilder answer = new StringBuilder();
+					if(dividend < 0 ^ divider < 0) {
+						answer.append('-');
+					}
+					answer.append(integerPart);
+					answer.append('.');
+					answer.append(fractionPart);
+					System.out.println(answer);
+				} else {
+						System.out.println("bad data format line");
+					continue;
 				}
 			}
 			scanner.close();
@@ -47,31 +65,25 @@ public class Fraction {
 		}
 	}
 	
-	private static StringBuilder convertFraction(int nominator, int denominator, 
-			int radix) {
-		StringBuilder sb = new StringBuilder('.');
-		int rest = nominator;
-		boolean isPeriodic = false;
-		List<Integer> restDump = new ArrayList<Integer>(rest);
+	private static StringBuilder translateFraction(long radix, 
+			long nominator, long denominator) {
+		StringBuilder sb = new StringBuilder();
+		List<Long> savedRests = new ArrayList<Long>();
+		long rest = nominator;
+		savedRests.add(rest);
 		while(rest != 0) {
 			nominator = rest * radix;
+			int digit = (int)(nominator / denominator);
+			sb.append(DIGITS[digit]);
 			rest = nominator % denominator;
-			for(int i = 0; i < restDump.size(); i++) {
-				if(restDump.get(i) == rest) {
-					char[] parenthesis = {'('};
-					sb.insert(i, parenthesis, 0, 1);
-					isPeriodic = true;
-					break;
+			for(int digitPos = 0; digitPos < savedRests.size(); digitPos++) {
+				if(savedRests.get(digitPos) == rest) {
+					sb.insert(digitPos, '(');
+					sb.append(')');
+					return sb;
 				}
 			}
-			if(isPeriodic) {
-				sb.append(')');
-				break;
-			} else {
-				int digit = nominator / denominator;
-				sb.append(DIGITS[digit]);
-				restDump.add(rest);
-			}
+			savedRests.add(rest);
 		}
 		return sb;
 	}
